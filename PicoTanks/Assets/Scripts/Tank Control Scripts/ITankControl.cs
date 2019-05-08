917c0fee124d9a3cf3964ead4f5c536a467fbf9e
Jacob Van Wagoner <veedubforlife@gmail.com>
2019-05-07 19:11:18 -0700

    Created first interface for script

---
+++ PicoTanks/Assets/Scripts/Tank Control Scripts/ITankControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface ITankControl : ITankBody, ITankGun
+{
+    
+}
+
+public interface ITankBody
+{
+    void Move(double Xinput, double Yinput);
+}
+
+public interface ITankGun
+{
+    void Aim(double Xinput, double Yinput);
+}
